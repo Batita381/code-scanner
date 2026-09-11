b76558b338d51239f83a22570e7ac6361724b919
Batita381 <gatita381@gmail.com>
2026-09-11 15:28:56 +0100

    Grocy PID
    Attempting to API a product barcode to first get a Grocy PID.

---
+++ src/main/java/com/budiyev/android/codescanner/AppCompatActivity.java
@@ -0,0 +1,9 @@
+package com.budiyev.android.codescanner;
+
+import android.os.Bundle;
+
+public class AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        
+    }
+}
---
+++ src/main/java/com/budiyev/android/codescanner/MainActivity.java
@@ -0,0 +1,66 @@
+package com.budiyev.android.codescanner;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.budiyev.android.codescanner.databinding.ActivityMainBinding;
+import com.google.zxing.Result;
+
+public class MainActivity extends com.budiyev.android.codescanner.AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    private void setContentView(int activityMain) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
